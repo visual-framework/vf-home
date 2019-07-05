d7ece009609781e22dffd19a73c2d9bd51347b68
Ken Hawkins <khawkins98@gmail.com>
2019-07-05 09:27:35 +0200

    Update site name, tweak h4 spacing in embl-grid

--- src/site/_data/siteConfig.js
+++ src/site/_data/siteConfig.js
@@ -1,7 +1,7 @@
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "The Visual Framework 2.0",
+    title: "Welcome to the Visual Framework 2.0",
     short_description: 'A front-end toolkit to quickly build better life science websites',
     url: "https://visual-framework.github.io/vf-welcome/",
     author: "Visual Framework system",
