96144ef81bf6623940217ef3371f1f4fbb7a5936
Ken Hawkins <khawkins98@gmail.com>
2019-09-04 13:36:49 +0100

    Update dependencies

---
+++ undefined/scripts/scripts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+/*
+ *
+ * scripts.css
+ * The Visual Framework kitchen sink of JavaScript.
+ * Import this as a quick way to get *everything*,
+ *
+ */
+vfBanner.vfBanner();
+vfMasthead.vfMastheadSetStyle();
+vfTabs.vfTabs();
+emblConditionalEdit.emblConditionalEdit();
+emblContentHubLoader.emblContentHub();
+emblBreadcrumbsLookup.emblBreadcrumbs();
+// No default invokation
