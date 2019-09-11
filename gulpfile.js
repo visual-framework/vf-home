26ba725f9da1fc7a78b8b2b31b3e28738f192b3f
Ken Hawkins <khawkins98@gmail.com>
2019-09-11 14:23:10 +0200

    Use update vf-local-override approach

--- gulpfile.js
+++ gulpfile.js
@@ -33,9 +33,6 @@ const componentPath = path.resolve('.', global.vfComponentPath).replace(/\\/g, '
 const componentDirectories = config.vfConfig.vfComponentDirectories || ['vf-core-components'];
 const buildDestionation = path.resolve('.', global.vfBuildDestination).replace(/\\/g, '/');
 
-// Some Gulp tasks live in their own files, for the sake of clarity.
-require('require-dir')('./gulp-tasks');
-
 // HACK: in vf-core@beta.2 a bug was introduced where it would erronously try to generate a version from the component template, this works around it by renaming the file
 // this can be removed once beta.3 is released
 
@@ -62,10 +59,8 @@ process.argv.push('--config=eleventy.js');
 
 // Watch folders for changess
 gulp.task('watch', function() {
-  gulp.watch(['./src/components/**/*.scss'], gulp.parallel('vf-css'));
+  gulp.watch(['./src/components/**/*.scss', '!./src/components/**/package.variables.scss'], gulp.parallel('vf-css'));
   gulp.watch(['./src/components/**/*.js'], gulp.parallel('vf-scripts'));
-  gulp.watch(['./src/scss/**/*.scss','./src/scss/*.scss'], gulp.parallel('css'));
-  gulp.watch(['./src/js/**/*.js','./src/js/*.js'], gulp.parallel('js'));
 });
 
 gulp.task('elventy-set-to-serve', function(done) {
@@ -85,8 +80,7 @@ gulp.task('elventy-set-to-build', function(done) {
   done();
 });
 
-// Run eleventy, but only after we wait for fractal to bootstrap
-// @todo: consider if this could/should be two parallel gulp tasks
+// Run Eleventy, but only after we wait for Fractal to start
 gulp.task('eleventy', function(done) {
   global.vfBuilderPath   = __dirname + '/build/vf-core-components';
   global.vfDocsPath      = __dirname + '/node_modules/\@visual-framework/vf-eleventy--extensions/fractal/docs';
@@ -105,7 +99,7 @@ let fractalBuildMode = 'build';
 gulp.task('build', gulp.series(
   'component-bug-hack',
   'vf-clean',
-  gulp.parallel('css','js','vf-css','vf-scripts','vf-component-assets'),
+  gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
   'elventy-set-to-build',
   'eleventy'
 ));
@@ -114,7 +108,7 @@ gulp.task('build', gulp.series(
 gulp.task('dev', gulp.series(
   'component-bug-hack',
   'vf-clean',
-  gulp.parallel('css','js','vf-css','vf-scripts','vf-component-assets'),
+  gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
   'elventy-set-to-serve',
   'eleventy',
   'watch'
