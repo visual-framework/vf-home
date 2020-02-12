8d23f001608132a652a2d02939c221cb1efe7d37
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-12 09:44:09 +0000

    Deploy visual-framework/vf-welcome to github.com/visual-framework/vf-welcome.git:gh-pages

---
+++ _data/siteConfig.js
@@ -0,0 +1,10 @@
+// Some various reusable configuration
+module.exports = {
+  siteInformation: {
+    title: "Welcome to the Visual Framework 2.0",
+    short_description: 'A front-end toolkit to quickly build better life science websites',
+    url: "https://visual-framework.github.io/vf-welcome/",
+    author: "Visual Framework system",
+    email: "ken.hawkins@embl.de",
+  }
+};
